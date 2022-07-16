a044a60e00b0837cc4ce306df02d79571e078d4a
Tom <tom.lucero@me.com>
2022-07-16 08:10:57 -0600

    adds gitignore

---
+++ C++/HackerRankProblems/HackerRankProblems/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HackerRankProblems
+//
+//  Created by Tom Lucero on 7/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
